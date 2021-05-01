1da9657b73203f0a8f57c00b9a8b6a1ab8acb354
HadasKedem <HadasKedem@DESKTOP-1468UU4>
2021-05-01 14:08:32 +0300

    get news from new york times and stash the head lines in array

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, request, jsonify, json
+from flask_cors import CORS
+from random import randrange
+
 from model.predict import Model
+from newsController import getNews
 
 app = Flask(__name__)
+CORS(app)
 
-model = Model("saved_models/510250_snli-0.733-0.811_mnli_matched-0.601-0.933_mnli_mismatched-0.608-0.925"
-              ".9258981491734342")
+# model = Model("saved_models/510250_snli-0.733-0.811_mnli_matched-0.601-0.933_mnli_mismatched-0.608-0.925"
+#               ".9258981491734342")
 
 
 @app.route('/', methods=["POST"])
 def hello():
     body = request.get_json()
-    prediction = model.predict(body['premise'], body['hypothesis'])
-    return {"prediction": prediction}
+    print(body)
+    prediction = randrange(100) # TODO get prediction from getNews
+    print(getNews(body['news']))
+    #model.predict(body['premise'], body['hypothesis'])
+    return  {"prediction": prediction}
---
+++ newsController.py
@@ -0,0 +1,22 @@
+import array
+import requests
+from model.predict import Model
+
+
+def getNews(hypothesis):
+    headlines = []
+    key = "mkmf1ZFfJzxR1gmt2GUW0JppZXKDHg9M"
+    url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + hypothesis + "&api-key=" + key
+    result = requests.get(url)
+
+    articles = (result.json())["response"]["docs"]
+    for art in articles:
+        headlines.append(art['headline']['main'])
+
+    for premise in headlines:
+        print(premise)
+        # perdict =  model.predict(premise, hypothesis) //TODO once the model will be loaded
+        # perdict // TODO average the prediction and send a percentage
+
+    percentage = 0
+    return percentage
