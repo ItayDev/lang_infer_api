2d7b6373dd8e2334a1461c751617a84322d0b375
Or Shachar <orsh>
2021-06-05 13:20:22 +0300

    Fixing true tweets ratio to all

--- models/pattern/PatternModelAnalyzer.py
+++ models/pattern/PatternModelAnalyzer.py
@@ -1,4 +1,4 @@
-MIN_TRUE_THRESHOLD = 0.3
+MIN_TRUE_THRESHOLD = 0.5
 MAX_FALSE_THRESHOLD = 0.15
 
 """
@@ -36,7 +36,8 @@ The default probabilities for true or misleading texts are configured above.
     possibly constructed by zip of the texts and probabilities such as:
     (("Trump is lying", 0.9), ("I like pasta", 0.6), ("peace in the middle east", 0.1))
     
-:return a list that has only definite probabilities of either true or false
+:return a grade in the range of [-1, 1] indication how truthful are the texts of the account,
+when -1 indicating complete liar while 1 indicting the most honest person
 """
 def grade_account(results_with_probs,
                   lower_threshold=MIN_TRUE_THRESHOLD,
@@ -49,9 +50,10 @@ def grade_account(results_with_probs,
                             for text_with_prob in results_with_probs
                             if text_with_prob[1] < upper_threshold)
 
-    if false_texts_count + true_texts_count == 0:
+    total_tweets = false_texts_count + true_texts_count
+
+    if total_tweets == 0:
         return 0
 
-    true_or_false_multiplier = 1 if true_texts_count > false_texts_count else -1
-    absolute_grade = max(true_texts_count, false_texts_count) / (false_texts_count + true_texts_count)
-    return true_or_false_multiplier * absolute_grade
+    true_tweet_ratio = true_texts_count / total_tweets
+    return 2 * true_tweet_ratio - 1
